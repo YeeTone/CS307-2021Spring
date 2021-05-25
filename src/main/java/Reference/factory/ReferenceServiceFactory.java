140cada8be75bac94bfcc9c0c8861d2d48b3363d
YeeTone <64548919+YeeTone@users.noreply.github.com>
2021-05-25 22:15:55 +0800

    Add files via upload

---
+++ src/main/java/Reference/factory/ReferenceServiceFactory.java
@@ -0,0 +1,6 @@
+package Reference.factory;
+
+import cn.edu.sustech.cs307.factory.ServiceFactory;
+
+public class ReferenceServiceFactory extends ServiceFactory {
+}
---
+++ src/main/java/Reference/service/ReferenceCourseService.java
@@ -0,0 +1,114 @@
+package Reference.service;
+
+import Reference.util.DBDataSource;
+import cn.edu.sustech.cs307.dto.Course;
+import cn.edu.sustech.cs307.dto.CourseSection;
+import cn.edu.sustech.cs307.dto.CourseSectionClass;
+import cn.edu.sustech.cs307.dto.Student;
+import cn.edu.sustech.cs307.dto.prerequisite.Prerequisite;
+import cn.edu.sustech.cs307.exception.IntegrityViolationException;
+import cn.edu.sustech.cs307.service.CourseService;
+
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.DayOfWeek;
+import java.util.List;
+
+@ParametersAreNonnullByDefault
+public class ReferenceCourseService implements CourseService {
+
+    @Override
+    public void addCourse(String courseId, String courseName,
+                          int credit, int classHour, Course.CourseGrading grading,
+                          @Nullable Prerequisite prerequisite) {
+
+    }
+
+    @Override
+    public int addCourseSection(String courseId, int semesterId, String sectionName, int totalCapacity) {
+        Connection con= DBDataSource.getConnection();
+        try{
+            String sql
+                    ="insert into coursesection" +
+                    "(courseId, semesterId, sectionName, totalCapacity)" +
+                    " values(?,?,?,?);";
+
+            PreparedStatement preparedStatement=con.prepareStatement(sql);
+
+            DBDataSource.beginTransaction(con);
+            preparedStatement.setString(1,courseId);
+            preparedStatement.setInt(2,semesterId);
+            preparedStatement.setString(3,sectionName);
+            preparedStatement.setInt(4,totalCapacity);
+
+            preparedStatement.executeUpdate(sql,PreparedStatement.RETURN_GENERATED_KEYS);
+            DBDataSource.commitTransaction(con);
+            ResultSet rs= preparedStatement.getGeneratedKeys();
+
+            if(rs.next()){
+                DBDataSource.close(con);
+                return rs.getInt(3);
+            }else {
+                DBDataSource.close(con);
+                throw new IntegrityViolationException();
+            }
+        }catch (SQLException e){
+            DBDataSource.close(con);
+            throw new IntegrityViolationException();
+        }
+    }
+
+    @Override
+    public int addCourseSectionClass(int sectionId, int instructorId, DayOfWeek dayOfWeek, List<Short> weekList, short classStart, short classEnd, String location) {
+        return 0;
+    }
+
+    @Override
+    public void removeCourse(String courseId) {
+
+    }
+
+    @Override
+    public void removeCourseSection(int sectionId) {
+
+    }
+
+    @Override
+    public void removeCourseSectionClass(int classId) {
+
+    }
+
+    @Override
+    public List<Course> getAllCourses() {
+        return null;
+    }
+
+    @Override
+    public List<CourseSection> getCourseSectionsInSemester(String courseId, int semesterId) {
+        return null;
+    }
+
+    @Override
+    public Course getCourseBySection(int sectionId) {
+        return null;
+    }
+
+    @Override
+    public List<CourseSectionClass> getCourseSectionClasses(int sectionId) {
+        return null;
+    }
+
+    @Override
+    public CourseSection getCourseSectionByClass(int classId) {
+        return null;
+    }
+
+    @Override
+    public List<Student> getEnrolledStudentsInSemester(String courseId, int semesterId) {
+        return null;
+    }
+}
