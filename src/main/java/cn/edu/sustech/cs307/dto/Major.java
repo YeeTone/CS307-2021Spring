f824fbea054ebfaa885bc6af61c5a09594c44e9a
YeeTone <64548919+YeeTone@users.noreply.github.com>
2021-06-03 11:56:49 +0800

    Add files via upload

--- src/main/java/cn/edu/sustech/cs307/dto/Major.java
+++ src/main/java/cn/edu/sustech/cs307/dto/Major.java
@@ -1,7 +1,22 @@
 package cn.edu.sustech.cs307.dto;
 
+import java.util.Objects;
+
 public class Major {
     public int id;
     public String name;
     public Department department;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Major major = (Major) o;
+        return id == major.id && Objects.equals(name, major.name) && Objects.equals(department, major.department);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, department);
+    }
 }
